c3c0f77be83648e8f27103fe3acd515f81b50984
Hamade <mmdarrah@hotmail.com>
2020-11-09 13:33:31 +0100

    get all the user from github api with axios is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import NavBar from '../src/components/layout/NavBar';
-
+import Users from '../src/components/users/Users';
+import axios from 'axios';
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(
+        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLINT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLINT_SECRET}`
+      )
+      .then((res) => setUsers(res.data), setLoading(false));
+  }, []);
+
   return (
     <div className='App'>
       <NavBar title='Github Finder' icon='fab fa-github' />
+      <div className='container'>
+        <Users loading={loading} users={users} />
+      </div>
     </div>
   );
 }
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 
-export class NavBar extends Component {
-  static defaultProps = {
-    title: 'Title',
-    icon: 'fab fa-react',
-  };
+const NavBar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: propTypes.string,
-    icon: propTypes.string,
-  };
-
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+NavBar.defaultProps = {
+  title: 'Title',
+  icon: 'fab fa-react',
+};
+NavBar.propTypes = {
+  title: propTypes.string,
+  icon: propTypes.string,
+};
 
 export default NavBar;
